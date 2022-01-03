7523c35f46b00b97d641852e8575fa380cfef4bc
chewyt <yeetian.chew@gmail.com>
2022-01-03 23:40:36 +0800

    Added Spring boot template

---
+++ project/src/main/java/chewyt/project/SsfDay13Application.java
@@ -0,0 +1,13 @@
+package chewyt.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfDay13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfDay13Application.class, args);
+	}
+
+}
---
+++ project/src/test/java/chewyt/project/SsfDay13ApplicationTests.java
@@ -0,0 +1,13 @@
+package chewyt.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfDay13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
